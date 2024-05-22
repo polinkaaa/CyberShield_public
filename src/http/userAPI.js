2b314db90db673255e768e41e4c11b7ba6881ad4
polinkaaa <polina_kond@mail.ru>
2024-05-22 14:40:01 +0300

    change

--- src/http/userAPI.js
+++ src/http/userAPI.js
@@ -6,12 +6,12 @@ const headers ={
 }/**//*$host*//**/
 
 export const registration = async (nickname, email, password) => {
-    const {data} = await axios.post('http://localhost:5000/api/user/registration', {nickname, email, password, role: 'ADMIN'}, headers)
-    return jwtDecode(data.token)
+    //const {data} = await axios.post('http://localhost:5000/api/user/registration', {nickname, email, password, role: 'ADMIN'}, headers)
+    //return jwtDecode(data.token)
 }
 export const login = async (email, password) => {
-    const {data} = await $host.post('api/user/login', {email, password})
-    return jwtDecode(data.token)
+   // const {data} = await $host.post('api/user/login', {email, password})
+   // return jwtDecode(data.token)
 }
 export const check = async () => {
     const response = await $host.post('api/user/registration')
