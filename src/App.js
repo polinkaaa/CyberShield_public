a30e4421b4e479027aea879b479867a3527f1809
polinkaaa <polina_kond@mail.ru>
2024-05-22 14:59:54 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from "react";
-import Main from './pages/Main'
-import AppRouter from "./components/AppRouter";
+import NavBar from "../components/NavBar";
+import { useNavigate } from "react-router-dom"
+import {observer} from "mobx-react-lite"
+import { CHAT_ROUTE, TASKS_ROUTE, THEORY_ROUTE } from "../utils/consts";
 
 const App = () => {
+    const history = useNavigate()
     return (
-        <Main>
-            <AppRouter />
-        </Main>
-    )
+        <div>
+            <NavBar />
+            <div className="menu_flex">
+                    <div className="menu_box" onClick={()=>history(THEORY_ROUTE)}>
+                        <p className="menu_title">Изучай теорию</p>
+                        <div className="menu_photo_1"></div>
+                    </div>
+                    <div className="menu_box" onClick={()=>history(TASKS_ROUTE)}>
+                        <p className="menu_title">Решай задания</p>
+                        <div className="menu_photo_2"></div>
+                    </div>
+                    <div className="menu_box" onClick={()=>history(CHAT_ROUTE)}>
+                        <p className="menu_title">Общайся в чате</p>
+                        <div className="menu_photo_3"></div>
+                    </div>
+                </div>
+        </div>
+    );
 }
 
 export default App;
